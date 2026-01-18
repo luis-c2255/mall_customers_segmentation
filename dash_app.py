b0918fe25433d054db8614cadbbe8cf4db55b6ce
luis-c2255 <lmodesto63@gmail.com>
2026-01-18 05:18:44 +0100

    Update dash_app.py

--- dash_app.py
+++ dash_app.py
@@ -8,6 +8,26 @@ st.markdown("""
             border: 1px solid #4cc9a6 !important;
             border-radius: 5px;
         }
+        /* Highlight the navigation links to look like buttons */
+        [data-testid="stSidebarNav"] ul {
+            padding-top: 2rem;
+        }
+        [data-testid="stSidebarNav"] li {
+            background-color: #1a2c42; /* Darker blue background */
+            border-radius: 10px;
+            margin-bottom: 10px;
+            border: 1px solid #4cc9a6; /* Your Mint Leaf color */
+            transition: 0.3s;
+        }
+        [data-testid="stSidebarNav"] li:hover {
+            background-color: #4cc9a6;
+            transform: translateX(5px);
+        }
+        [data-testid="stSidebarNav"] span {
+            color: white !important;
+            font-weight: bold;
+        }
+
     </style>
 """, unsafe_allow_html=True)
 
